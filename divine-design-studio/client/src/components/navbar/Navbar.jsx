6f54c2eaf10c6e422019bceb1e1e5575ee3e2acd
Michael Kithinji <michaelkithinji27@gmail.com>
2023-11-11 23:49:33 +0300

    Complete toggle button

--- divine-design-studio/client/src/components/navbar/Navbar.jsx
+++ divine-design-studio/client/src/components/navbar/Navbar.jsx
@@ -50,10 +50,11 @@ function Navbar() {
             <img className="Logo" src={DDS} alt="" />
           </Link>
         </div>
-        <div className="links" ref={navRef}>
-          <nav className="nav-items">
+        <div className="links" >
+          <nav className="nav-items" ref={navRef}>
             <span>Home</span>
             <span>About</span>
+            <span>Our Clients</span>
             <span>Products </span>
             <span>Services </span>
 
@@ -98,12 +99,29 @@ function Navbar() {
             )}
 
             <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-              <FaTimes />
+            <svg
+      viewBox="0 0 470 1000"
+      fill="red"
+      height="2em"
+      width="1em"
+    >
+      <path d="M452 656c12 12 18 26.333 18 43s-6 31-18 43c-12 10.667-26.333 16-43 16s-31-5.333-43-16L234 590 102 742c-12 10.667-26.333 16-43 16s-31-5.333-43-16C5.333 730 0 715.667 0 699s5.333-31 16-43l138-156L16 342C5.333 330 0 315.667 0 299s5.333-31 16-43c12-10.667 26.333-16 43-16s31 5.333 43 16l132 152 132-152c12-10.667 26.333-16 43-16s31 5.333 43 16c12 12 18 26.333 18 43s-6 31-18 43L314 500l138 156" />
+    </svg>
             </button>
           </nav>
 
           <button className="nav-btn" onClick={showNavbar}>
-            <FaBars />
+          <svg
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      height="1em"
+      width="2em"
+    >
+      <path
+        fillRule="evenodd"
+        d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"
+      />
+    </svg>
           </button>
         </div>
       </div>
