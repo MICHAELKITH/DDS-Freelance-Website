b66934a69fd0b7dd8d107b0c178538ab97dcf881
Michael Kithinji <michaelkithinji27@gmail.com>
2023-11-11 22:06:23 +0300

    Implementing navbar navigation bar

--- divine-design-studio/client/src/components/navbar/Navbar.jsx
+++ divine-design-studio/client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import newRequest from "../../utils/newRequest";
-import DDS from "/img/DDS.png"
+import DDS from "/img/DDS.png";
 
 import "./Navbar.scss";
 
 function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
 
@@ -41,59 +47,70 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <Link className="link" to="/">
-            {/* <span className="text">Divine Design</span><br />
-            <span className="text">Studio</span> */}
-            <img className ="Logo" src={DDS} alt="" />
+            <img className="Logo" src={DDS} alt="" />
           </Link>
-          {/* <span className="dot">.</span> */}
         </div>
-        <div className="links">
-          <span>Home</span>
-          <span>About</span>
-          <span>Products </span>
-          <span>Services </span>
-          {!currentUser?.isSeller && <span>Become a Freelancer</span>}
-          {currentUser ? (
-            <div className="user" onClick={() => setOpen(!open)}>
-              <img src={currentUser.img || "/img/noavatar.jpg"} alt="" />
-              <span>{currentUser?.username}</span>
-              {open && (
-                <div className="options">
-                  {currentUser.isSeller && (
-                    <>
-                      <Link className="link" to="/mygigs">
-                        Gigs
-                      </Link>
-                      <Link className="link" to="/add">
-                        Add New Gig
-                      </Link>
-                    </>
-                  )}
-                  <Link className="link" to="/orders">
-                    Orders
-                  </Link>
-                  <Link className="link" to="/messages">
-                    Messages
-                  </Link>
-                  <Link className="link" onClick={handleLogout}>
-                    Logout
-                  </Link>
-                </div>
-              )}
-            </div>
-          ) : (
-            <>
-              <Link to="/login" className="link">Sign in</Link>
-              <Link className="link" to="/register">
-                <button>Create Account</button>
-              </Link>
-            </>
-          )}
+        <div className="links" ref={navRef}>
+          <nav className="nav-items">
+            <span>Home</span>
+            <span>About</span>
+            <span>Products </span>
+            <span>Services </span>
+
+            {!currentUser?.isSeller && <span>Become a Freelancer</span>}
+            {currentUser ? (
+              <div className="user" onClick={() => setOpen(!open)}>
+                <img src={currentUser.img || "/img/noavatar.jpg"} alt="" />
+                <span>{currentUser?.username}</span>
+                {open && (
+                  <div className="options">
+                    {currentUser.isSeller && (
+                      <>
+                        <Link className="link" to="/mygigs">
+                          Gigs
+                        </Link>
+                        <Link className="link" to="/add">
+                          Add New Gig
+                        </Link>
+                      </>
+                    )}
+                    <Link className="link" to="/orders">
+                      Orders
+                    </Link>
+                    <Link className="link" to="/messages">
+                      Messages
+                    </Link>
+                    <Link className="link" onClick={handleLogout}>
+                      Logout
+                    </Link>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <>
+                <Link to="/login" className="link">
+                  Sign in
+                </Link>
+                <Link className="link" to="/register">
+                  <button>Create Account</button>
+                </Link>
+              </>
+            )}
+
+            <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+              <FaTimes />
+            </button>
+          </nav>
+
+          <button className="nav-btn" onClick={showNavbar}>
+            <FaBars />
+          </button>
         </div>
       </div>
-      {(active || pathname !== "/") && (
+
+      {/* {(active || pathname !== "/") && (
         <>
-          {/* <hr />
+          <hr />
           <div className="menu">
             <Link className="link menuLink" to="/">
               Marketing 
@@ -112,9 +129,9 @@ function Navbar() {
             </Link>
             
           </div>
-          <hr /> */}
+          <hr />
         </>
-      )}
+      )} */}
     </div>
   );
 }
